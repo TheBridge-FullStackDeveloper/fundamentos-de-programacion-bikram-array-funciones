5d51b7a60424c42c76094e0b87574a249475412e
Brujitari <natimontesg2@mail.com>
2022-02-26 23:10:51 +0100

    feat: ejercicios 1, 2, 3, 4 hechos

---
+++ index.js
@@ -0,0 +1,14 @@
+
+//1
+const arrayVacio = []
+
+//2
+const arrayNumeros = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+//3
+const arrayNumerosPares = [ 0, 2, 4, 6, 8]
+
+//4
+const arrayBidimensional = [[ 0, 1, 2], [ 'a', 'b', 'c']]
+
+//5
